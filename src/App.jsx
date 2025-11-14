e4ab8a0193e1ae4e00ad8446b07c8968ae91149b
shahariar270 <mdshahariar102@gmail.com>
2025-11-14 11:27:38 +0600

    basic class constructor setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Settings } from './pages/Settings'
+import { Animation } from './utils/animation'
 
 function App() {
+
+  useEffect(() => {
+    const element = document.getElementById('box')
+
+
+    if (element) {
+      const anim = new Animation(element);
+      anim.animate();
+    }
+
+  }, [])
 
   return (
     <React.Fragment>
       <div className="container">
         <div className="wrapper">
+          <div className="box" id='box'></div>
         </div>
         <div className="settings">
           <Settings />
---
+++ src/utils/animation.js
@@ -0,0 +1,12 @@
+import gsap from "gsap";
+
+
+export class Animation {
+    constructor(element) {
+        this.element = element;
+        console.log(this.element);
+    }
+    animate() {
+      
+    }
+}
