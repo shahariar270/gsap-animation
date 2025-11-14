7ea11979b1026111b8a31bf43459a43de3901045
shahariar270 <mdshahariar102@gmail.com>
2025-11-14 20:00:53 +0600

    animation type setu[s

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,14 +1,46 @@
 import gsap from "gsap";
-
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 export class Animation {
     constructor(element, attribute) {
         this.element = element;
         this.attribute = attribute;
 
-        const { scale } = this.attribute;
     }
     animate() {
+        if (!this.attribute) return;
+        const { animationType, triggerType } = this.attribute;
+
+        let animationProps;
+
+        if (animationType === 'fade') {
+            animationProps = { opacity: 0, y: 50, duration: 1, ease: "power2.out" };
+        } else if (animationType === '3dmove') {
+            animationProps = { opacity: 0, x: -200, rotationY: 180, duration: 1.2, ease: "power3.out" };
+        } else if (animationType === 'custom') {
+            animationProps = { opacity: 0, x: 100, y: -100, scale: 0.5, rotation: 360, duration: 1.5, ease: "elastic.out(1,0.5)" };
+        }
+
+        if (triggerType === 'scroll') {
+            gsap.from(this.element, {
+                ...animationProps,
+                scrollTrigger: {
+                    trigger: this.element,
+                    start: "top 80%",
+                    toggleActions: "play none none reverse"
+                }
+            });
+        }
+        else if (triggerType === 'hover') {
+            this.element.addEventListener('mouseenter', () => gsap.from(this.element, animationProps));
+        }
+        else if (triggerType === 'click') {
+            this.element.addEventListener('click', () => gsap.from(this.element, animationProps));
+        }
+        else if (triggerType === 'load') {
+            gsap.from(this.element, animationProps);
+        }
 
     }
-}
+}   
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,16 @@
 export const getAttribute = () => ({
-  scale: 1.1,
-  duration: 0.3,
-  yoyo: true,
-  repeat: 1
+  x: 0,
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  delay: 0,
+  ease: "power3.out",
+  rotation: 0,
+  scale: 1,
+  stagger: 0,
+  repeat: 0,
+  yoyo: false,
+  transformOrigin: "50% 50%",
+  animationType:'fade',
+  triggerType:'hover'
 });
